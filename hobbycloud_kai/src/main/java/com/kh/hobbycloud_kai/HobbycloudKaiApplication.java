4bdc606159424a78b227014bf6585230deb6c3c5
Daanta <daanta@naver.com>
2022-02-02 18:53:19 +0900

    220202_1853 스프링부트 프로젝트 생성

---
+++ hobbycloud_kai/src/main/java/com/kh/hobbycloud_kai/HobbycloudKaiApplication.java
@@ -0,0 +1,13 @@
+package com.kh.hobbycloud_kai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbycloudKaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbycloudKaiApplication.class, args);
+	}
+
+}
